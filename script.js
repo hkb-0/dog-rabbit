118ec0b6e755d7bfd3ef54a44bdbb1ab3aaf9336
candy91degozaru-eng <candy91de.gozaru@gmail.com>
2026-05-29 17:35:06 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const images = [
+  "01.jpg",
+  "02.jpg",
+  "03.jpg",
+  "04.jpg",
+  "05.jpg",
+  "06.jpg",
+  "07.jpg",
+  "08.jpg",
+  "09.jpg",
+  "10.jpg"
+];
+
+const slider = document.getElementById("slider");
+const frame = document.getElementById("frame");
+
+slider.addEventListener("input", () => {
+  frame.src = images[slider.value];
+});
